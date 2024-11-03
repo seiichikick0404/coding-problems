a3026febd44892e7a509e8131ff459668865e3ef
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-11-03 23:00:53 +0900

    update code

--- free.py
+++ free.py
@@ -1,10 +1,22 @@
-S = input()
-N = int(input())
 
-menus = input().split()
-for menu in menus:
-    if menu == S:
-        print("Yes")
-        exit()
+total_odd = 0
+total = 0
+for i in range(4):
+    for j in range(4):
+        for k in range(4):
+            i_str = str(i)
+            j_str = str(j)
+            k_str = str(k)
+            ans = int(i_str + j_str + k_str)
+            if ans % 2 != 0:
+                total_odd += 1
 
-print("No")
+            if ans > 230:
+                total += 1
+
+
+
+print(total_odd)
+print(total)
+
+
