b7f6f3dc4ba2faa61f50b485c564b92410cf8451
seiichi0404 <yamayamaseisei1834@gmail.com>
2025-04-27 09:13:23 +0900

    リファクタ

--- practice_new/A - Triple Four/A - Triple Four.rb
+++ practice_new/A - Triple Four/A - Triple Four.rb
@@ -1,19 +1,15 @@
 n = gets.chomp.to_i
 numbers = gets.chomp.split.map(&:to_i)
 
-prev = numbers[0]
 counter = 1
-numbers.drop(1).each do |curr|
-  if curr == prev
-    counter += 1
-    if counter == 3
-      puts "Yes"
-      exit
-    end
-  else
-    counter = 1
+
+numbers.each_cons(2) do |prev, curr|
+  counter = (prev == curr) ? counter + 1 : 1
+
+  if counter == 3
+    puts "Yes"
+    exit
   end
-  prev = curr
 end
 
 puts "No"
