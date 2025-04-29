5e64cdfe8c9199d0d64f246a8166f5d22e14cdb0
seiichi0404 <yamayamaseisei1834@gmail.com>
2025-04-30 07:49:38 +0900

    A問題追加

---
+++ practice_new/A - Doors in the Center/tempCodeRunnerFile.rb
@@ -0,0 +1,11 @@
+ans = ""
+
+n.times do |i|
+    center = n / 2
+    if n.even?
+        ans += (i < center -1) || (i == center) ? "=" : "-"
+    else
+        ans += (i == center) ? "=": "-"
+    end
+end
+puts ans
---
+++ practice_new/A - Thermometer/A - Thermometer.rb
@@ -0,0 +1,9 @@
+x = gets.chomp.to_f
+
+if x >= 37.5 && x < 38.0
+    puts "2"
+elsif x >= 38.0
+    puts "1"
+else
+    puts "3"
+end
